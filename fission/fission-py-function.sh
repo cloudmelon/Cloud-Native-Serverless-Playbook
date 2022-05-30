f8717cc301746b0b938c05e9ebe0c3aa7d05f237
Melony Qin <melqin@microsoft.com>
2022-05-30 15:19:16 -0700

    add message sender

--- fission/fission-py-function.sh
+++ fission/fission-py-function.sh
@@ -5,7 +5,7 @@ $ fission env create --name python --image fission/python-env
 $ curl -LO https://github.com/cloudmelon/cloud-native-spark/blob/main/my-spark-app/sample_pys_park.py
 
 # Upload your function code to fission
-$ fission function create --name hello-py --env python --code hello.py
+$ fission function create --name hello-py --env python --code sample_pys_park.py
 
 # Test your function.  This takes about 100msec the first time.
 $ fission function test --name hello-py
---
+++ message-sender/MessageSender/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System; // Namespace for Console output
+using System.Configuration; // Namespace for ConfigurationManager
+using System.Threading.Tasks; // Namespace for Task
+using Azure.Identity;
+using Azure.Storage.Queues; // Namespace for Queue storage types
+using Azure.Storage.Queues.Models; // Namespace for PeekedMessage
+
+Console.WriteLine("Greetings !");
+
+// Get the connection string from app settings
+string connectionString = ConfigurationManager.AppSettings["StorageConnectionString"];
+
+// Instantiate a QueueClient which will be used to create and manipulate the queue
+QueueClient queueClient = new QueueClient(connectionString, "functions-queue");
+
+// Create the queue if it doesn't already exist
+queueClient.CreateIfNotExists();
+
+if (queueClient.Exists())
+{
+    // Create a message and add it to the queue.
+    for (int i = 0; i < 5000; i++)
+    {
+  
+        // Send a message to the queue
+        queueClient.SendMessage("Hello geeks " + i);
+        Console.WriteLine("Message No." + i + " added ");
+    }
+
+}
+
+Console.WriteLine("Peace !");
+Console.ReadKey();
